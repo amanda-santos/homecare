1ea12b0ea58a32e0907f326515643c3014929a78
amanda-santos <amandasf0200@gmail.com>
2021-11-26 19:41:53 -0300

    Add created successfully page

--- frontend/pages/caregivers/created-successfully.tsx
+++ frontend/pages/caregivers/created-successfully.tsx
@@ -1,10 +1,25 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import { Button } from "../../components";
 import { CreatedSuccessfullyWrapper } from "../../styles/Pages/CreatedSuccessfully";
 
 const CreatedSuccess: NextPage = () => {
   return (
     <CreatedSuccessfullyWrapper>
-      Created successfully
+      <Image
+        src="/images/check.svg"
+        alt="Cadastro salvo!"
+        height="120"
+        width="120"
+      />
+      <h1>Cadastro salvo!</h1>
+      <p>
+        Tudo certo, seu cadastro está na nossa lista de cuidadores. <br />
+        Agora é só ficar de olho no seu WhatsApp.
+      </p>
+      <a href={"/caregivers"}>
+        <Button label="Acessar lista" color="secondary" icon="arrow-right" />
+      </a>
     </CreatedSuccessfullyWrapper>
   );
 };
--- frontend/styles/Pages/CreatedSuccessfully/CreatedSuccessfullyWrapper.ts
+++ frontend/styles/Pages/CreatedSuccessfully/CreatedSuccessfullyWrapper.ts
@@ -8,4 +8,18 @@ export const CreatedSuccessfullyWrapper = styled.div`
   background-color: var(--primary);
   height: 100vh;
   width: 100vw;
+  color: var(--white);
+
+  h1 {
+    font-size: 4.8rem;
+    margin: 1.6rem 0 0 0;
+    line-height: 4rem;
+  }
+
+  p {
+    font-size: 1.6rem;
+    font-weight: 300;
+    text-align: center;
+    margin: 4rem 0;
+  }
 `;
